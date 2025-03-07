08316bed69fdfe85e74c4f329e86c2c48e1f98e4
Sena Durgut <senadurgut18@gmail.com>
2025-03-07 10:40:49 +0300

    API integrated

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,60 +1,28 @@
-import 'package:http/http.dart' as http;
-import 'package:water_reminder/models/api_model.dart';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl =
-      "https://dummyjson.com/auth/login"; // Kendi API adresin
+  final String baseUrl = "https://dummyjson.com";
 
-  // Kullanıcı giriş yapma fonksiyonu
-  static Future<String?> loginUser(String username, String password) async {
-    final url = Uri.parse('$baseUrl/auth/login'); // API endpoint’in
+  Future<Map<String, dynamic>?> login(String username, String password) async {
+    final url = Uri.parse("$baseUrl/auth/login");
+
     try {
       final response = await http.post(
         url,
-        headers: {'Content-Type': 'application/json'},
+        headers: {"Content-Type": "application/json"},
         body: jsonEncode({"username": username, "password": password}),
       );
 
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        String accessToken = data['token']; // Token alanını API'ne göre ayarla
-        return accessToken;
+        return jsonDecode(response.body);
       } else {
-        return null; // Hata durumunda null döndür
+        print("Giriş başarısız: ${response.body}");
+        return null;
       }
     } catch (e) {
-      print('Login Hatası: $e');
+      print("Hata oluştu: $e");
       return null;
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-Future<ApiModel> apiCall() async {
-  final response = await http.post(
-    Uri.parse('https://dummyjson.com/auth/login'),
-  );
-  if (response.statusCode == 200) {
-    return ApiModel.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load record');
-  }
-}
-
-
-*/
--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,67 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:water_reminder/views/pages/profile_page.dart';
 import 'package:water_reminder/services/api_services.dart';
 
 // ignore: must_be_immutable
 class LoginPage extends StatefulWidget {
-  LoginPage({super.key});
-
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController usernameController = TextEditingController();
-
   final TextEditingController passwordController = TextEditingController();
 
-  String? accessToken;
+  final ApiService apiService = ApiService();
 
   void login() async {
-    String? token = await ApiService.loginUser(
-      usernameController.text,
-      passwordController.text,
-    );
+    String username = usernameController.text;
+    String password = passwordController.text;
 
-    if (token != null) {
-      setState(() {
-        accessToken = token;
-      });
-      print("Giriş başarılı! Token: $accessToken");
+    final response = await apiService.login(username, password);
+
+    if (response != null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Giriş başarılı: ${response['token']}")),
+      );
     } else {
-      print("Giriş başarısız!");
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Giriş başarısız")));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(title: Text('Login')),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: usernameController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 labelText: 'Username',
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 16),
+            SizedBox(height: 16),
             TextField(
               obscureText: true,
               controller: passwordController,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 24),
-            ElevatedButton(onPressed: login, child: const Text('Login')),
+            SizedBox(height: 24),
+            ElevatedButton(onPressed: login, child: Text('Login')),
           ],
         ),
       ),
