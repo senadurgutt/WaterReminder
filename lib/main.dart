dd9c3d538b6be97acf746094f38fdeb751d7af69
Sena Durgut <senadurgut18@gmail.com>
2025-03-10 15:04:38 +0300

    deleted notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:water_reminder/services/notification.dart';
 import 'package:water_reminder/utils/colors.dart';
 import 'package:water_reminder/views/pages/login_page.dart';
 import 'controllers/record_controller.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.initialize();
+void main() {
   runApp(MyApp());
 }
 
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,8 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_timezone
-  local_notifier
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
